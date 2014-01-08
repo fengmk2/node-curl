19c37af91ea5194a69d4750e5934c974a3d27719
David Oliveira <d.oliveira@prozone.org>
2014-01-08 16:37:40 +0000

    Curl version changed to 7.34.0

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 mkdir -p deps
 cd deps
 rm -rf curl
-curl -o curl-7.27.0.tar.gz http://curl.haxx.se/download/curl-7.27.0.tar.gz
-tar zxf curl-7.27.0.tar.gz
-mv curl-7.27.0 curl
+curl -o curl-7.34.0.tar.gz http://curl.haxx.se/download/curl-7.34.0.tar.gz
+tar zxf curl-7.34.0.tar.gz
+mv curl-7.34.0 curl
 cd curl
 ./configure --without-ssl --disable-shared && make
 cd ../..
